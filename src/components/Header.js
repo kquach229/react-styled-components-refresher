0015691f12ea1e5d2ab47cc08e8430eccc01c7db
kquach229 <kquach229>
2022-12-15 00:30:48 -0500

    save

---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+import { StyledHeader } from "./styles/Header.styled"
+export default function Header() {
+  return (
+    <StyledHeader showBlue={true}>
+      <h1>Header</h1>
+    </StyledHeader>
+  )
+}
